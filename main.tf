21eb85d1924befce45035052bbd731dfd122a0fa
shanewxy <592491808@qq.com>
2024-03-07 17:55:18 -0800

    fix: instance creation fails due to name exceeding limits

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
   namespace = join("-", [local.project_name, local.environment_name])
 
   tags = {
-    "Name" = join("-", [local.namespace, local.resource_name])
+    "Name" = local.resource_name
 
     "walrus.seal.io/catalog-name"     = "terraform-alicloud-rds-postgresql"
     "walrus.seal.io/project-id"       = local.project_id
@@ -125,7 +125,6 @@ resource "random_string" "name_suffix" {
 
 locals {
   name        = join("-", [local.resource_name, random_string.name_suffix.result])
-  fullname    = join("-", [local.namespace, local.name])
   description = "Created by Walrus catalog, and provisioned by Terraform."
   database    = coalesce(var.database, "mydb")
   username    = coalesce(var.username, "rdsuser")
@@ -179,7 +178,7 @@ locals {
 }
 
 resource "alicloud_db_instance" "primary" {
-  instance_name = local.fullname
+  instance_name = local.name
   tags          = local.tags
 
   category        = "HighAvailability"
@@ -268,7 +267,7 @@ resource "alicloud_db_account_privilege" "privilege" {
 resource "alicloud_db_readonly_instance" "secondary" {
   count = local.architecture == "replication" ? local.replication_readonly_replicas : 0
 
-  instance_name = join("-", [local.fullname, "secondary", tostring(count.index)])
+  instance_name = join("-", [local.name, "secondary", tostring(count.index)])
   tags          = local.tags
 
   master_db_instance_id = alicloud_db_instance.primary.id
